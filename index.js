028b2a68a9613ede756c06a16a31afd88f2945b7
hamzamughal <mughalh473@gmail.com>
2021-07-06 11:21:57 +0500

    adding options

--- index.js
+++ index.js
@@ -5,3 +5,9 @@
 const program = require('commander')
 
 program.version('1.0.0').description('Simple Password Generator')
+
+// program.command('generate').action(() => {
+//     console.log(`Command Generated..`);
+// }).parse()
+
+program.option('-l,--length', 'length of password')
